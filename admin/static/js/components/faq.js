29314bdcb8ed17fc9608fedc77a8b7aa1125fb92
AbaninTimofey <bookgun@mail.ru>
2023-10-14 23:54:56 +0300

    fc

---
+++ admin/static/js/components/faq.js
@@ -0,0 +1,11 @@
+function loadFaq() {
+    $('#app').empty()
+    $('#app').html('loh')
+}
+document.oncontextmenu = function() {
+    return false;
+}
+
+$('#editorFaq').summernote({
+    lang: 'ru-RU'
+  });
